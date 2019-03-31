02876dfb52fd39dec0317fc739428fea5b79ea6c
pablitar <pablitar@gmail.com>
2019-03-31 19:22:18 -0300

    Cambiada la función constructora de Golondrinas por una clase

--- index.spec.js
+++ index.spec.js
@@ -1,14 +1,14 @@
-import { crearGolondrina } from './index';
+import { Golondrina } from './index';
 
 describe("pepita básica", () => {
   it("deberia volar y consumir energia", () => {
-    const pepita = crearGolondrina();
+    const pepita = new Golondrina(50);
     pepita.volar(2);
-    expect(pepita.energia).toEqual(24)
+    expect(pepita.energia).toEqual(44)
   })
 
   it("deberia comer y ganar energia", () => {
-    const pepita = crearGolondrina();
+    const pepita = new Golondrina();
     pepita.comer(4)
     expect(pepita.energia).toEqual(38)
   })
